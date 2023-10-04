f73110e54d3d5228316997eaa8e9bd038e5522ea
Os-humble-man <Oscarkanangila01@gmail.com>
2023-10-04 19:48:14 +0200

    login ui

--- app/src/main/java/com/example/kazi/Login.java
+++ app/src/main/java/com/example/kazi/Login.java
@@ -3,6 +3,8 @@ package com.example.kazi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+//        ImageView imageView = findViewById(R.id.myImageView);
+
+//        int newHWidth = 200;
+//        int newHeight = 300;
+//
+//        ViewGroup.LayoutParams layoutParams = imageView.getLayoutParams();
+//        layoutParams.width = newHWidth;
+//        layoutParams.height = newHeight;
+//
+//        imageView.setLayoutParams(layoutParams);
     }
 }
--- app/src/main/java/com/example/kazi/Splashscreen.java
+++ app/src/main/java/com/example/kazi/Splashscreen.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import kotlinx.coroutines.Delay;
 
@@ -13,13 +14,16 @@ public class Splashscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
-        Intent intent = new Intent(this, Login.class);
-        try {
-            Thread.sleep(4000);
-        }catch (Exception e){
-            System.out.println(Thread.currentThread().getName());
-        }
-        startActivity(intent);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splashscreen.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
     }
 
 }
