84d734a4db26ee707865ca0419992c0f7c2a64e4
Os-humble-man <Oscarkanangila01@gmail.com>
2023-10-13 14:56:59 +0200

    add home page

--- app/src/main/java/com/example/kazi/Login.java
+++ app/src/main/java/com/example/kazi/Login.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Login extends AppCompatActivity {
@@ -11,16 +12,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Button signup = findViewById(R.id.signup);
         setContentView(R.layout.activity_login);
-//        ImageView imageView = findViewById(R.id.myImageView);
-
-//        int newHWidth = 200;
-//        int newHeight = 300;
-//
-//        ViewGroup.LayoutParams layoutParams = imageView.getLayoutParams();
-//        layoutParams.width = newHWidth;
-//        layoutParams.height = newHeight;
-//
-//        imageView.setLayoutParams(layoutParams);
     }
 }
